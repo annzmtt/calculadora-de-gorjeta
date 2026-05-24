1de3a030103085447af56a8854f9940ad55c7a6d
anna <annzmtt@gmail.com>
2026-05-24 19:45:09 -0300

    quarto commit do java

--- script.js
+++ script.js
@@ -30,6 +30,23 @@ botoesGorjeta.forEach(botao => {
     botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagem(evento) {
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+         }
+ })
+
+if(evento.target.value !== "") {
     porcentagem = parseFloat(evento.target.value) / 100
+} else {
+    porcentagem = 0
 }
+
+console.log(porcentagem)
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
+
